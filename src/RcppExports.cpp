91425eea9ad480eb52fd95d57c1ab29949025423
Mikkel Meyer Andersen <mikl@math.aau.dk>
2020-03-03 13:30:15 +0000

    version 0.2-2

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,61 +5,108 @@
 
 using namespace Rcpp;
 
+// score_rcpp
+Rcpp::IntegerVector score_rcpp(const Rcpp::IntegerVector& prof1, const Rcpp::IntegerVector& prof2, int numLoci, bool useWildCard, bool useRareAllele);
+RcppExport SEXP _DNAtools_score_rcpp(SEXP prof1SEXP, SEXP prof2SEXP, SEXP numLociSEXP, SEXP useWildCardSEXP, SEXP useRareAlleleSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Rcpp::IntegerVector& >::type prof1(prof1SEXP);
+    Rcpp::traits::input_parameter< const Rcpp::IntegerVector& >::type prof2(prof2SEXP);
+    Rcpp::traits::input_parameter< int >::type numLoci(numLociSEXP);
+    Rcpp::traits::input_parameter< bool >::type useWildCard(useWildCardSEXP);
+    Rcpp::traits::input_parameter< bool >::type useRareAllele(useRareAlleleSEXP);
+    rcpp_result_gen = Rcpp::wrap(score_rcpp(prof1, prof2, numLoci, useWildCard, useRareAllele));
+    return rcpp_result_gen;
+END_RCPP
+}
 // compare
-Rcpp::List compare(SEXP db, SEXP numLoci, SEXP bigHit, SEXP trace, SEXP single, SEXP useWildcard, SEXP useWildcardEffect, SEXP useRallele);
-RcppExport SEXP DNAtools_compare(SEXP dbSEXP, SEXP numLociSEXP, SEXP bigHitSEXP, SEXP traceSEXP, SEXP singleSEXP, SEXP useWildcardSEXP, SEXP useWildcardEffectSEXP, SEXP useRalleleSEXP) {
+Rcpp::List compare(const Rcpp::StringVector& DB, int numLoci, int bigHit, bool trace, int single, bool useWildcard, bool useWildcardEffect, bool useRallele);
+RcppExport SEXP _DNAtools_compare(SEXP DBSEXP, SEXP numLociSEXP, SEXP bigHitSEXP, SEXP traceSEXP, SEXP singleSEXP, SEXP useWildcardSEXP, SEXP useWildcardEffectSEXP, SEXP useRalleleSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type db(dbSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type numLoci(numLociSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type bigHit(bigHitSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type trace(traceSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type single(singleSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useWildcard(useWildcardSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useWildcardEffect(useWildcardEffectSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useRallele(useRalleleSEXP);
-    rcpp_result_gen = Rcpp::wrap(compare(db, numLoci, bigHit, trace, single, useWildcard, useWildcardEffect, useRallele));
+    Rcpp::traits::input_parameter< const Rcpp::StringVector& >::type DB(DBSEXP);
+    Rcpp::traits::input_parameter< int >::type numLoci(numLociSEXP);
+    Rcpp::traits::input_parameter< int >::type bigHit(bigHitSEXP);
+    Rcpp::traits::input_parameter< bool >::type trace(traceSEXP);
+    Rcpp::traits::input_parameter< int >::type single(singleSEXP);
+    Rcpp::traits::input_parameter< bool >::type useWildcard(useWildcardSEXP);
+    Rcpp::traits::input_parameter< bool >::type useWildcardEffect(useWildcardEffectSEXP);
+    Rcpp::traits::input_parameter< bool >::type useRallele(useRalleleSEXP);
+    rcpp_result_gen = Rcpp::wrap(compare(DB, numLoci, bigHit, trace, single, useWildcard, useWildcardEffect, useRallele));
     return rcpp_result_gen;
 END_RCPP
 }
-// score
-Rcpp::IntegerVector score(SEXP prof1, SEXP prof2, SEXP numLoci, SEXP useWildCard, SEXP useRareAllele);
-RcppExport SEXP DNAtools_score(SEXP prof1SEXP, SEXP prof2SEXP, SEXP numLociSEXP, SEXP useWildCardSEXP, SEXP useRareAlleleSEXP) {
+// compare_threaded
+Rcpp::List compare_threaded(const Rcpp::StringVector& DB, int numLoci, int bigHit, bool trace, int single, bool useWildcard, bool useWildcardEffect, bool useRallele);
+RcppExport SEXP _DNAtools_compare_threaded(SEXP DBSEXP, SEXP numLociSEXP, SEXP bigHitSEXP, SEXP traceSEXP, SEXP singleSEXP, SEXP useWildcardSEXP, SEXP useWildcardEffectSEXP, SEXP useRalleleSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type prof1(prof1SEXP);
-    Rcpp::traits::input_parameter< SEXP >::type prof2(prof2SEXP);
-    Rcpp::traits::input_parameter< SEXP >::type numLoci(numLociSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useWildCard(useWildCardSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useRareAllele(useRareAlleleSEXP);
-    rcpp_result_gen = Rcpp::wrap(score(prof1, prof2, numLoci, useWildCard, useRareAllele));
+    Rcpp::traits::input_parameter< const Rcpp::StringVector& >::type DB(DBSEXP);
+    Rcpp::traits::input_parameter< int >::type numLoci(numLociSEXP);
+    Rcpp::traits::input_parameter< int >::type bigHit(bigHitSEXP);
+    Rcpp::traits::input_parameter< bool >::type trace(traceSEXP);
+    Rcpp::traits::input_parameter< int >::type single(singleSEXP);
+    Rcpp::traits::input_parameter< bool >::type useWildcard(useWildcardSEXP);
+    Rcpp::traits::input_parameter< bool >::type useWildcardEffect(useWildcardEffectSEXP);
+    Rcpp::traits::input_parameter< bool >::type useRallele(useRalleleSEXP);
+    rcpp_result_gen = Rcpp::wrap(compare_threaded(DB, numLoci, bigHit, trace, single, useWildcard, useWildcardEffect, useRallele));
     return rcpp_result_gen;
 END_RCPP
 }
-// mcompare
-Rcpp::List mcompare(SEXP db, SEXP numLoci, SEXP bigHit, SEXP trace, SEXP single, SEXP numThreads, SEXP useWildcard, SEXP useWildcardEffect, SEXP useRallele);
-RcppExport SEXP DNAtools_mcompare(SEXP dbSEXP, SEXP numLociSEXP, SEXP bigHitSEXP, SEXP traceSEXP, SEXP singleSEXP, SEXP numThreadsSEXP, SEXP useWildcardSEXP, SEXP useWildcardEffectSEXP, SEXP useRalleleSEXP) {
+// convolve
+NumericVector convolve(NumericMatrix x);
+RcppExport SEXP _DNAtools_convolve(SEXP xSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type db(dbSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type numLoci(numLociSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type bigHit(bigHitSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type trace(traceSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type single(singleSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type numThreads(numThreadsSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useWildcard(useWildcardSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useWildcardEffect(useWildcardEffectSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type useRallele(useRalleleSEXP);
-    rcpp_result_gen = Rcpp::wrap(mcompare(db, numLoci, bigHit, trace, single, numThreads, useWildcard, useWildcardEffect, useRallele));
+    Rcpp::traits::input_parameter< NumericMatrix >::type x(xSEXP);
+    rcpp_result_gen = Rcpp::wrap(convolve(x));
+    return rcpp_result_gen;
+END_RCPP
+}
+// Pnm_locus
+NumericVector Pnm_locus(int m, double theta, NumericVector alleleProbs);
+RcppExport SEXP _DNAtools_Pnm_locus(SEXP mSEXP, SEXP thetaSEXP, SEXP alleleProbsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type m(mSEXP);
+    Rcpp::traits::input_parameter< double >::type theta(thetaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type alleleProbs(alleleProbsSEXP);
+    rcpp_result_gen = Rcpp::wrap(Pnm_locus(m, theta, alleleProbs));
+    return rcpp_result_gen;
+END_RCPP
+}
+// Pnm_all_cpp
+NumericMatrix Pnm_all_cpp(int numContrib, double theta, List loci);
+RcppExport SEXP _DNAtools_Pnm_all_cpp(SEXP numContribSEXP, SEXP thetaSEXP, SEXP lociSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type numContrib(numContribSEXP);
+    Rcpp::traits::input_parameter< double >::type theta(thetaSEXP);
+    Rcpp::traits::input_parameter< List >::type loci(lociSEXP);
+    rcpp_result_gen = Rcpp::wrap(Pnm_all_cpp(numContrib, theta, loci));
+    return rcpp_result_gen;
+END_RCPP
+}
+// generateCompositions
+List generateCompositions(int numContributors);
+RcppExport SEXP _DNAtools_generateCompositions(SEXP numContributorsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type numContributors(numContributorsSEXP);
+    rcpp_result_gen = Rcpp::wrap(generateCompositions(numContributors));
     return rcpp_result_gen;
 END_RCPP
 }
 // Prob
 NumericVector Prob(CharacterVector vstrCombs, NumericVector q, NumericVector R, double r, double t);
-RcppExport SEXP DNAtools_Prob(SEXP vstrCombsSEXP, SEXP qSEXP, SEXP RSEXP, SEXP rSEXP, SEXP tSEXP) {
+RcppExport SEXP _DNAtools_Prob(SEXP vstrCombsSEXP, SEXP qSEXP, SEXP RSEXP, SEXP rSEXP, SEXP tSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -71,4 +118,21 @@ BEGIN_RCPP
     rcpp_result_gen = Rcpp::wrap(Prob(vstrCombs, q, R, r, t));
     return rcpp_result_gen;
 END_RCPP
+}
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_DNAtools_score_rcpp", (DL_FUNC) &_DNAtools_score_rcpp, 5},
+    {"_DNAtools_compare", (DL_FUNC) &_DNAtools_compare, 8},
+    {"_DNAtools_compare_threaded", (DL_FUNC) &_DNAtools_compare_threaded, 8},
+    {"_DNAtools_convolve", (DL_FUNC) &_DNAtools_convolve, 1},
+    {"_DNAtools_Pnm_locus", (DL_FUNC) &_DNAtools_Pnm_locus, 3},
+    {"_DNAtools_Pnm_all_cpp", (DL_FUNC) &_DNAtools_Pnm_all_cpp, 3},
+    {"_DNAtools_generateCompositions", (DL_FUNC) &_DNAtools_generateCompositions, 1},
+    {"_DNAtools_Prob", (DL_FUNC) &_DNAtools_Prob, 5},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_DNAtools(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
--- src/profile.h
+++ src/profile.h
@@ -13,6 +13,7 @@
 /* 1.0      2013-10-23  Added extra arg to compare       JMC              */  
 /* 1.0                  method for locus by locus                         */
 /*                      scoring                                           */
+/* 1.1      2018-03-22  Added vector constructor         MMA              */
 
 #include <string>
 #include <sstream>
@@ -38,6 +39,7 @@ public:
   // constructors
 	Profile(int nLoci);
   Profile(int *pnProfile, int nLoci);
+  Profile(const std::vector<int>& profile);
 	Profile(string &strLine, int nLoci, char cDelim = '\t');
 
   // copy constructor
@@ -53,3 +55,4 @@ public:
   // printing code
   string toString(bool bShowHoms = false);
 };
+
